5a93cc67acf14ff81c1b3661641e649ac9e0b3f9
Jadson Santos <jadsoonbol@gmail.com>
2023-04-01 02:33:03 -0300

    pagina de mesas

--- src/mesas/Mesas.js
+++ src/mesas/Mesas.js
@@ -4,130 +4,153 @@ import "./mesas.css";
 
 function Mesas() {
 
-    // puxando atributos das mesas
+  // puxando atributos das mesas
 
-    const [myState, setMyState] = useState([{
-        id: 0, numero: 1, status: "livre", chamandoGarcom: false,
-        pedidos: ["2764", "2765"],
-        clientes: ["jeferson", "natan"],
-        conta: {
-            
-            produtos: [{nome: "hamburguer", quntidade: 2, valor: 30}, {nome: "cerveja", quantidade: 1, valor: 10}],
-            total: 70
-        }
-    },
-    {
-        id: 1, numero: 2, status: "ocupada", chamandoGarcom: false,
-        pedidos: ["2764", "2765"],
-        clientes: ["jadson", "natan"],
-        conta: {
-            
-            produtos: [{nome: "hamburguer", quntidade: 2, valor: 30}, {nome: "cerveja", quantidade: 1, valor: 10}],
-            total: 60
-        }
-    },
-    {
-        id: 2, numero: 3, status: "indisponivel", chamandoGarcom: false,
-        pedidos: ["2764", "2765"],
-        clientes: ["jadson", "natan"],
-        conta: {
-            produtos: [{nome: "hamburguer", quntidade: 2, valor: 30}, {nome: "cerveja", quantidade: 1, valor: 10}],
-            total: 50
-        }
+  const [myState, setMyState] = useState([{
+    id: 0, numero: 1, status: "livre", chamandoGarcom: false,
+    pedidos: ["2764", "2765"],
+    clientes: ["jeferson", "natan"],
+    conta: {
+
+      produtos: [{ id: 1, nome: "hamburguer", quantidade: 2, valor: 30 }, { id: 2, nome: "cerveja", quantidade: 1, valor: 10 }],
+      total: 70
     }
-    ]);
+  },
+  {
+    id: 1, numero: 2, status: "ocupada", chamandoGarcom: false,
+    pedidos: ["2764", "2765"],
+    clientes: ["jadson", "natan"],
+    conta: {
 
-    // função abrir e fechar poupup da conta
+      produtos: [{ id: 1, nome: "hamburguer", quantidade: 2, valor: 30 }, { id: 2, nome: "cerveja", quantidade: 1, valor: 10 }],
+      total: 60
+    }
+  },
+  {
+    id: 2, numero: 3, status: "indisponivel", chamandoGarcom: false,
+    pedidos: ["2764", "2765"],
+    clientes: ["jadson", "natan"],
+    conta: {
+      produtos: [{ id: 1, nome: "hamburguer", quantidade: 2, valor: 30 }, { id: 2, nome: "cerveja", quantidade: 1, valor: 10 }],
+      total: 50
+    }
+  }
+  ]);
 
-    const [conta, setConta] = React.useState("close");
-    const handleOpenConta = () => setConta("open");
-    const handleCloseConta = () => setConta("close");
+  // função abrir e fechar poupup da conta
 
-    // const style = {
-    //     position: 'absolute',
-    //     top: '50%',
-    //     left: '50%',
-    //     transform: 'translate(-50%, -50%)',
-    //     width: 400,
-    //     bgcolor: 'background.paper',
-    //     border: '2px solid #000',
-    //     boxShadow: 24,
-    //     p: 4,
-    // };
+  const [conta, setConta] = React.useState("close");
+  const handleOpenConta = () => setConta("open");
+  const handleCloseConta = () => setConta("close");
 
-//     const array = [{indice: 0, nome: "elemento1"},
-// {indice: 1, nome: "elemento2"},
-// {indice: 2, nome: "elemento3"}]
+  // const style = {
+  //     position: 'absolute',
+  //     top: '50%',
+  //     left: '50%',
+  //     transform: 'translate(-50%, -50%)',
+  //     width: 400,
+  //     bgcolor: 'background.paper',
+  //     border: '2px solid #000',
+  //     boxShadow: 24,
+  //     p: 4,
+  // };
+
+  //     const array = [{indice: 0, nome: "elemento1"},
+  // {indice: 1, nome: "elemento2"},
+  // {indice: 2, nome: "elemento3"}]
 
 
-//  useState para funções de abrir e fechar poupup da mesa
+  //  useState para funções de abrir e fechar poupup da mesa
 
-    const [elementoAtivo, setElementoAtivo] = useState(null);
+  const [elementoAtivo, setElementoAtivo] = useState(null);
 
-    const handleClick = (id) => {
-        setElementoAtivo(id);
-      }
+  const handleClick = (id) => {
+    setElementoAtivo(id);
+  }
 
-    
-// popupup da mesa
 
-      const Popup = ({ object }) => {
-        if (elementoAtivo !== object.id) {
-          return null;
-        }
-        
-        const handleClose = () => {
-          setElementoAtivo(null);
-          setConta("close");
-        };
-      
-        return (
-          <div className="poupupmesa">
-            <div className="title"><h1>Mesa {object.numero}</h1><button onClick={handleClose}>Fechar</button></div>
-            <div className="corpo">
-            <h2>Pedidos</h2>
-            {object.pedidos.map((pedido, id) => (
-        <a className="pedido" href="#" key={id}>{pedido}, </a>
-      ))}
+  // popupup da mesa
 
-      <h2>Clientes</h2>
-      {object.clientes.map((cliente, id) => (
-        <p className="cliente" href="#" key={id}>{cliente} </p>
-      ))}
-        <button onClick={handleOpenConta}>Conta</button>
-      <div id={conta}>
-        <ul>Conta</ul>
-        <li>{object.conta.total}</li>
-        <button onClick={handleCloseConta}>fechar</button>
+  const Popup = ({ object }) => {
+    if (elementoAtivo !== object.id) {
+      return null;
+    }
 
-      </div>
-
-      
-            </div>
-            
-          </div>
-        );
-      };
-
-      
-    // renderizando array de mesas
+    const handleClose = () => {
+      setElementoAtivo(null);
+      setConta("close");
+    };
 
     return (
-        <div className="mesas">
-            {myState.map((object, id) => (
-                <div >
-                <div id={object.status} className="mesa" onClick={() => handleClick(id)} key={id}>
-                    <h1 ><b>Mesa {object.numero}</b></h1>
-                    </div>
-                    <Popup object={object} />
-                </div>
-
-
+      <div className="poupupmesa">
+        <div className="title"><h1>Mesa {object.numero}</h1><button onClick={handleClose}>X</button></div>
+        <div className="corpo">
+          <h2>Pedidos</h2>
+          <div className="pedido">
+            {object.pedidos.map((pedido, id) => (
+              <p key={id}>{pedido}</p>
             ))}
+          </div>
 
+          <h2>Clientes</h2>
+          <div className="cliente">
+            {object.clientes.map((cliente, id) => (
+              <p key={id}>{cliente} </p>
+            ))}
+          </div>
+          <div className="footer">
+            <button onClick={handleOpenConta}>Conta</button>
+          </div>
+          <div className="printable-modal">
+            <div id={conta} className="contaImprimir">
+              <h2>Mesa {object.numero}</h2>
 
+              {/* produtos da conta */}
+              <table >
+                <tbody className="tableConta">
+                  {object.conta.produtos.map((produto, id) => (
+                    <tr key={id}>
+                      <td>{produto.quantidade}</td>
+                      <td>{produto.nome}</td>
+                      <td>{produto.valor * produto.quantidade}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+              <p>Valor total: {object.conta.total}</p>
+              <div className="footerConta">
+                <button onClick={handleCloseConta}>Fechar</button>
+                <button onClick={() => window.print()}>Imprimir</button>
+              </div>
+
+            </div>
+
+          </div>
         </div>
+
+      </div>
     );
+  };
+
+
+  // renderizando array de mesas
+
+  return (
+    <div className="mesas">
+      {myState.map((object, id) => (
+        <div >
+          <div id={object.status} className="mesa" onClick={() => handleClick(id)} key={id}>
+            <h1 ><b>Mesa {object.numero}</b></h1>
+          </div>
+          <Popup object={object} />
+        </div>
+
+
+      ))}
+
+
+    </div>
+  );
 }
 
 {/* <Modal 
