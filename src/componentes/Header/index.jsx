b9023e00f64242eba65df12a9b7b06a179b981b3
unknown <jadsoonbol@gmail.com>
2023-09-08 23:30:53 -0300

    logout

--- src/componentes/Header/index.jsx
+++ src/componentes/Header/index.jsx
@@ -1,17 +1,22 @@
 /* eslint-disable prettier/prettier */
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { Container } from './styles'
 import { FaBars } from 'react-icons/fa'
 import Sidebar from '../Sidebar'
 import './header.css';
 import logomarcaEmpresa from '../../assets/logomarca.png';
+import { AuthContext } from '../../contexts/auth';
 
 const Header = () => {
-  const [sidebar, setSidebar] = useState(false)
+  const [sidebar, setSidebar] = useState(false);
 
-  const showSiderbar = () => setSidebar(!sidebar)
+  const showSiderbar = () => setSidebar(!sidebar);
 
-  const user = 'Jadson';
+  const { user, logout } = useContext(AuthContext);
+
+  const handleLogout = () => {
+    logout();
+  }
 
   return (
     <Container className="menubar">
@@ -23,7 +28,7 @@ const Header = () => {
                     <img className='logoarcafooter' width='100px' src={logomarcaEmpresa} alt="Logo" />   
                 </div>
                 <div className="divUser">
-                    <p>Olá {user}</p>
+                    <p>Olá {user.usuario.nome}</p><p onClick={handleLogout} className='logout'>Sair</p>
                 </div>
     </Container>
   )
