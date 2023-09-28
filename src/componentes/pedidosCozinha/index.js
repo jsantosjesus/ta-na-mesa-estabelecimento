5785d9495366081005a6725aa44905d33303ac6c
unknown <jadsoonbol@gmail.com>
2023-09-27 21:42:35 -0300

    working in cozinha (PEDIDO)

--- src/componentes/pedidosCozinha/index.js
+++ src/componentes/pedidosCozinha/index.js
@@ -4,40 +4,35 @@ import MuiAccordion from '@mui/material/Accordion';
 import MuiAccordionSummary from '@mui/material/AccordionSummary';
 import MuiAccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
+import './pedidosCozinha.css';
 
 const Accordion = styled((props) => (
   <MuiAccordion disableGutters elevation={0} square {...props} />
 ))(({ theme }) => ({
   border: `1px solid ${theme.palette.divider}`,
   '&:not(:last-child)': {
-    borderBottom: 0,
+    borderBottom: 0
   },
   '&:before': {
-    display: 'none',
-  },
+    display: 'none'
+  }
 }));
 
-const AccordionSummary = styled((props) => (
-  <MuiAccordionSummary
-    {...props}
-  />
-))(({ theme }) => ({
+const AccordionSummary = styled((props) => <MuiAccordionSummary {...props} />)(({ theme }) => ({
   backgroundColor:
-    theme.palette.mode === 'dark'
-      ? 'rgba(255, 255, 255, .05)'
-      : 'rgba(0, 0, 0, .03)',
+    theme.palette.mode === 'dark' ? 'rgba(255, 255, 255, .05)' : 'rgba(0, 0, 0, .03)',
   flexDirection: 'row-reverse',
   '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
-    transform: 'rotate(90deg)',
+    transform: 'rotate(90deg)'
   },
   '& .MuiAccordionSummary-content': {
-    marginLeft: theme.spacing(1),
-  },
+    marginLeft: theme.spacing(1)
+  }
 }));
 
 const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
   padding: theme.spacing(2),
-  borderTop: '1px solid rgba(0, 0, 0, .125)',
+  borderTop: '1px solid rgba(0, 0, 0, .125)'
 }));
 
 export default function CustomizedAccordions() {
@@ -55,28 +50,15 @@ export default function CustomizedAccordions() {
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            01 - Refrigerante 1L R$10.99<br />
-            01 - Hamburguer (sem saladda)<br />
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
-        <AccordionSummary aria-controls="panel2d-content" id="panel2d-header">
-          <Typography>Pedido 47 (Mesa 12)</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            01 - Pizza de calabresa - R$ 79.99
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
-        <AccordionSummary aria-controls="panel3d-content" id="panel3d-header">
-          <Typography>Pedido 48 (Mesa 25)</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            01 - Cerveja Skol 300ml - R$ 4.00
+            <p className="pedidoProduto">01 - Refrigerante 1L - R$10.99</p>
+            <p className="pedidoProduto">01 - Hamburguer - (sem salada) - R$6.00</p>
+            <p className="totalPedido">
+              <b>TOTAL: R$16.99</b>
+            </p>
+            <div className="botoesPedido">
+              <button className="confirmarPedido">Confirmar</button>
+              <button className="cancelarPedido">Pedir Alteração</button>
+            </div>
           </Typography>
         </AccordionDetails>
       </Accordion>
